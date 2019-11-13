cbb5eef04239937595ca9fc3198ea75599259f1c
Grégory D'Angelo <dangelo.gregory@gmail.com>
2019-11-13 20:05:13 +0100

    feat: create Board component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 // Components
-import CardList from './components/CardList';
+import Board from './components/Board';
 // Stylesheets
 import './App.css';
 
 const data = [
   {
     id: 0,
-    description: 'This is my first card...'
+    cards: [
+      { id: 0, description: 'This is my first card...' },
+      { id: 1, description: 'This is a test' }
+    ]
   },
   {
     id: 1,
-    description: 'This is a test'
-  }
+    cards: [
+      { id: 0, description: 'A card for my second list' },
+      { id: 1, description: 'Another one!' }
+    ]
+  },
 ];
 
 function App() {
   return (
     <div className="App">
-      <CardList cards={data} />
+      <Board lists={data} />
     </div>
   );
 }
---
+++ src/components/Board.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+
+class Board extends Component {
+  render() {
+    return (
+      <div className="board">
+        <ol className="board-lists">
+          {
+            this.props.lists.map(list => (
+              <li key={list.id}>
+                <CardList cards={list.cards} />
+              </li>
+            ))
+          }
+        </ol>
+      </div>
+    );
+  }
+};
+
+export default Board;
