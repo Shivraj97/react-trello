c6313d5890db12bbad0bedd4919b1715c805e2ee
Grégory D'Angelo <dangelo.gregory@gmail.com>
2019-11-23 16:21:16 +0100

    styles: use styled components for Divider

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const DividerContainer = styled.hr`
+  background-color: rgba(9,30,66,.13);
+  border: 0;
+  height: 1px;
+`;
 
 const Divider = () => (
-  <hr className="divider" />
+  <DividerContainer />
 );
 
 export default Divider;
