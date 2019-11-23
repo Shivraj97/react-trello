66c799e13c854eed0eb55633ca69795cd986b46c
Grégory D'Angelo <dangelo.gregory@gmail.com>
2019-11-23 17:17:42 +0100

    styles: use styled components for Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import Tag from './Tag';
 
-class Card extends Component {
-  render() {
-    let tags = null;
-    if (this.props.tags) {
-      tags = (
-        <div className="tags">
-        {
-          this.props.tags.map((tag, i) => (
-            <Tag key={i} text={tag} />
-          ))
-        }
-        </div>
-      );
-    }
-    
-    return (
-      <div className="card">
-        { tags }
-        <p className="card-description">{'#' + this.props.id + ' ' + this.props.description}</p>
-      </div>
-    );
+const CardContainer = styled.div`
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 rgba(9,30,66,.25);
+  cursor: pointer;
+  margin-bottom: 8px;
+  min-height: 20px;
+  max-height: 200px;
+  padding: 10px;
+  margin-right: 5px;
+
+  &:hover {
+    background-color: #f4f5f7;
   }
+`;
+
+const TagsContainer = styled.div`
+  margin-bottom: 5px;
+`;
+
+const Card = ({ id, description = "", tags = [] }) => {
+  console.log(id, description, tags);
+  return (
+  <CardContainer>
+    {
+      tags.length > 0 &&
+      <TagsContainer>
+      {
+        tags.map((tag, i) => (
+          <Tag key={i} text={tag} />
+        ))
+      }
+      </TagsContainer>
+    }
+    <p>{'#' + id + ' ' + description}</p>
+  </CardContainer>
+  );
 };
 
 export default Card;
